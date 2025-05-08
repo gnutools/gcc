ab67b7152603d16471c856636c0a4f2a3fbc934f
Mikael Morin <mikael@gcc.gnu.org>
2025-05-08 18:30:34 +0200

    Correction régression id-24.f

--- gcc/fortran/trans-array.cc
+++ gcc/fortran/trans-array.cc
@@ -3878,6 +3878,7 @@ gfc_conv_array_ref (gfc_se * se, gfc_array_ref * ar, gfc_expr *expr,
 
   tree elt_size;
   bool use_array_ref = array_ref_safe_p (decl, &elt_size);
+  elt_size = fold_convert_loc (input_location, gfc_array_index_type, elt_size);
 
   tree off = gfc_conv_array_offset (decl);
   if (use_array_ref)
