f32fd706880ece0fd6cb31b90d41dc43f5bd32dc
Mikael Morin <mikael@gcc.gnu.org>
2025-05-14 10:40:17 +0200

    Correction régression unsigned_23

--- gcc/tree.cc
+++ gcc/tree.cc
@@ -13133,12 +13133,22 @@ array_ref_up_bound (tree exp)
   if (domain_type && TYPE_MAX_VALUE (domain_type))
     {
       tree val = SUBSTITUTE_PLACEHOLDER_IN_EXPR (TYPE_MAX_VALUE (domain_type), exp);
-      if (TREE_OPERAND (exp, 2))
-        return fold_build2_loc (EXPR_LOCATION (exp), PLUS_EXPR,
-				TREE_TYPE (val), val,
-				fold_convert_loc (EXPR_LOCATION (exp),
-						  TREE_TYPE (val),
-						  TREE_OPERAND (exp, 2)));
+      tree low_bnd = TREE_OPERAND (exp, 2);
+      if (low_bnd)
+	{
+	  tree dom_min = TYPE_MIN_VALUE (domain_type);
+	  tree off = fold_build2_loc (EXPR_LOCATION (exp), MINUS_EXPR,
+				      TREE_TYPE (val),
+				      fold_convert_loc (EXPR_LOCATION (low_bnd),
+							TREE_TYPE (val),
+							low_bnd),
+				      fold_convert_loc (EXPR_LOCATION (dom_min),
+							TREE_TYPE (val),
+							dom_min));
+
+	  return fold_build2_loc (EXPR_LOCATION (exp), PLUS_EXPR,
+				  TREE_TYPE (val), val, off);
+	}
       else
 	return val;
     }
