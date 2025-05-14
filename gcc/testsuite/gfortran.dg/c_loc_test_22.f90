459a08fffa781673507973594dfe46aeca46972e
Mikael Morin <mikael@gcc.gnu.org>
2025-05-14 19:22:54 +0200

    Mise à jour motif dump c_loc_test_22.f90

--- gcc/testsuite/gfortran.dg/c_loc_test_22.f90
+++ gcc/testsuite/gfortran.dg/c_loc_test_22.f90
@@ -15,9 +15,9 @@ subroutine sub(xxx, yyy)
   ptr4 = c_loc (yyy(5:))
 end
 ! { dg-final { scan-tree-dump-not " _gfortran_internal_pack" "original" } }
-! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) &\\(.xxx.\[0-9\]+\\)\\\[0\\\];" 1 "original" } }
-! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) &\\(.xxx.\[0-9\]+\\)\\\[D.\[0-9\]+ \\* 4\\\];" 1 "original" } }
-! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) yyy.\[0-9\]+;" 1 "original" } }
-! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) yyy.\[0-9\]+ \\+ \\(sizetype\\) \\(D.\[0-9\]+ \\* 16\\);" 1 "original" } }
+! { dg-final { scan-tree-dump-times {parm.[0-9]+\.data = \(void .\) xxx.[0-9]+;} 1 "original" } }
+! { dg-final { scan-tree-dump-times {parm.[0-9]+\.data = \(void .\) \(&\(\*xxx.[0-9]+\)\[0\](?:\{lb: 0 sz: 4\})? \+ \(sizetype\) \(D.[0-9]+ \* 4\)\);} 1 "original" } }
+! { dg-final { scan-tree-dump-times {parm.[0-9]+\.data = \(void .\) yyy.[0-9]+;} 1 "original" } }
+! { dg-final { scan-tree-dump-times {parm.[0-9]+\.data = \(void .\) \(yyy.[0-9]+ \+ \(sizetype\) \(D.[0-9]+ \* 4\)\);} 1 "original" } }
 
 ! { dg-final { scan-tree-dump-times "D.\[0-9\]+ = parm.\[0-9\]+.data;\[^;]+ptr\[1-4\] = D.\[0-9\]+;" 4 "original" } }
