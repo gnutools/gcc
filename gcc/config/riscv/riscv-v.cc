58ac5282231a694ea04492421d4c824cc89be515
Pan Li <pan2.li@intel.com>
2025-05-25 17:13:09 +0800

    RISC-V: Combine vec_duplicate + vxor.vv to vxor.vx on GR2VR cost
    This patch would like to combine the vec_duplicate + vxor.vv to the
    vxor.vx.  From example as below code.  The related pattern will depend
    on the cost of vec_duplicate from GR2VR.  Then the late-combine will
    take action if the cost of GR2VR is zero, and reject the combination
    if the GR2VR cost is greater than zero.
    
    Assume we have example code like below, GR2VR cost is 0.
    
      #define DEF_VX_BINARY(T, OP)                                        \
      void                                                                \
      test_vx_binary (T * restrict out, T * restrict in, T x, unsigned n) \
      {                                                                   \
        for (unsigned i = 0; i < n; i++)                                  \
          out[i] = in[i] OP x;                                            \
      }
    
      DEF_VX_BINARY(int32_t, |)
    
    Before this patch:
      10   │ test_vx_binary_or_int32_t_case_0:
      11   │     beq a3,zero,.L8
      12   │     vsetvli a5,zero,e32,m1,ta,ma
      13   │     vmv.v.x v2,a2
      14   │     slli    a3,a3,32
      15   │     srli    a3,a3,32
      16   │ .L3:
      17   │     vsetvli a5,a3,e32,m1,ta,ma
      18   │     vle32.v v1,0(a1)
      19   │     slli    a4,a5,2
      20   │     sub a3,a3,a5
      21   │     add a1,a1,a4
      22   │     vxor.vv v1,v1,v2
      23   │     vse32.v v1,0(a0)
      24   │     add a0,a0,a4
      25   │     bne a3,zero,.L3
    
    After this patch:
      10   │ test_vx_binary_or_int32_t_case_0:
      11   │     beq a3,zero,.L8
      12   │     slli    a3,a3,32
      13   │     srli    a3,a3,32
      14   │ .L3:
      15   │     vsetvli a5,a3,e32,m1,ta,ma
      16   │     vle32.v v1,0(a1)
      17   │     slli    a4,a5,2
      18   │     sub a3,a3,a5
      19   │     add a1,a1,a4
      20   │     vxor.vx v1,v1,a2
      21   │     vse32.v v1,0(a0)
      22   │     add a0,a0,a4
      23   │     bne a3,zero,.L3
    
    The below test suites are passed for this patch.
    * The rv64gcv fully regression test.
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-v.cc (expand_vx_binary_vec_dup_vec): Add
    	new case for XOR op.
    	(expand_vx_binary_vec_vec_dup): Diito.
    	* config/riscv/riscv.cc (riscv_rtx_costs): Ditto.
    	* config/riscv/vector-iterators.md: Add new op or to no_shift_vx_ops.
    
    Signed-off-by: Pan Li <pan2.li@intel.com>
    (cherry picked from commit df691d8de1f4ea21763ca579bc6fb1fab38512da)

--- gcc/config/riscv/riscv-v.cc
+++ gcc/config/riscv/riscv-v.cc
@@ -5576,6 +5576,7 @@ expand_vx_binary_vec_dup_vec (rtx op_0, rtx op_1, rtx op_2,
     case PLUS:
     case AND:
     case IOR:
+    case XOR:
       icode = code_for_pred_scalar (code, mode);
       break;
     case MINUS:
@@ -5604,6 +5605,7 @@ expand_vx_binary_vec_vec_dup (rtx op_0, rtx op_1, rtx op_2,
     case MINUS:
     case AND:
     case IOR:
+    case XOR:
       icode = code_for_pred_scalar (code, mode);
       break;
     default:
