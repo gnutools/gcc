b7606609c895ee2428a862bfa17e91a42146d0d7
Mikael Morin <mikael@gcc.gnu.org>
2025-07-15 19:13:46 +0200

    Utilisation gfc_clear_descriptor pour initialiser les résultats de type class
    Prise en charge type polymorphe
    
    Correction gfc_clear_descriptor
    
    Utilisation gfc_symbol_attr

--- gcc/fortran/trans-descriptor.cc
+++ gcc/fortran/trans-descriptor.cc
@@ -673,22 +673,29 @@ gfc_get_descriptor_offsets_for_info (const_tree desc_type, tree *data_off,
 void
 gfc_clear_descriptor (stmtblock_t *block, gfc_symbol *sym, tree descr)
 {
+  symbol_attribute attr = gfc_symbol_attr (sym);
+
   /* NULLIFY the data pointer for non-saved allocatables, or for non-saved
      pointers when -fcheck=pointer is specified.  */
-  if (sym->attr.allocatable
-      || (sym->attr.pointer && (gfc_option.rtcheck & GFC_RTCHECK_POINTER)))
+  if (attr.allocatable
+      || (attr.pointer && (gfc_option.rtcheck & GFC_RTCHECK_POINTER)))
     {
       gfc_conv_descriptor_data_set (block, descr, null_pointer_node);
-      if (flag_coarray == GFC_FCOARRAY_LIB && sym->attr.codimension)
+      if (flag_coarray == GFC_FCOARRAY_LIB && attr.codimension)
 	gfc_conv_descriptor_token_set (block, descr, null_pointer_node);
     }
 
   tree etype;
 
-  gcc_assert (sym->as && sym->as->rank>=0);
+  gfc_array_spec *as;
+  if (sym->ts.type == BT_CLASS)
+    as = CLASS_DATA (sym)->as;
+  else
+    as = sym->as;
+
+  gcc_assert (as && as->rank >= 0);
   etype = gfc_get_element_type (TREE_TYPE (descr));
   gfc_conv_descriptor_dtype_set (block, descr,
-				 gfc_get_dtype_rank_type (sym->as->rank,
-							  etype));
+				 gfc_get_dtype_rank_type (as->rank, etype));
 }
 
