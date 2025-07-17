9da47f74cdd4c715825dfb1482a9cc18c79ec6d0
Michael Meissner <meissner@linux.ibm.com>
2025-07-17 19:11:58 -0400

    Add support for -mcpu=future
    This patch adds the support that can be used in developing GCC support for
    future PowerPC processors.
    
    This support is done by adding support for CPU ISA bits that are set directly
    via the -mcpu=<xxx> option, without having a -m<option> bit.
    
    2025-07-17  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config.gcc (powerpc*-*-*): Add support for configuration option
    	--with-cpu=future.
    	* config/rs6000/aix71.h (ASM_CPU_SPEC): Pass -mfuture to the assembler
    	if -mcpu=future was used.
    	* config/rs6000/aix72.h (ASM_CPU_SPEC): Likewise.
    	* config/rs6000/aix73.h (ASM_CPU_SPEC): Likewise.
    	* config/rs6000/driver-rs6000.cc (asm_names): Likewise.
    	* config/rs6000/rs6000-c.cc (rs6000_target_modify_macros): If
    	-mcpu=future, define _ARCH_FUTURE.  Add CPU ISA flag bit argument.
    	(rs6000_cpu_cpp_builtins): Likewise.
    	* config/rs6000/rs6000-cpus.def (FUTURE_MASKS_SERVER): New macro.
    	(RS6000_CPU_ISA): New optional macro.
    	(future cpu): Set up for -mcpu=future.
    	* config/rs6000/rs6000-opts.h (PROCESSOR_FUTURE): Define as
    	PROCESSOR_POWER11 for now.
    	* config/rs6000/rs6000-protos.h (rs6000_target_modify_macros): Add
    	argument for CPU ISA bits.
    	(CPU_ISA_MASK_FUTURE): New macro.
    	(rs6000_target_modify_macros_ptr): Likewise.
    	* config/rs6000/rs6000-tables.opt: Regenerate.
    	* config/rs6000/rs6000.cc (rs6000_target_modify_macros_ptr): Add
    	additional argument.
    	(rs6000_print_isa_options): Likewise.
    	(RS6000_CPU_ISA): New macro.
    	(DEBUG_FMT_WX): Update to print both isa flags and CPU isa flags.
    	(rs6000_debug_reg_global): Add support for CPU ISA options that are
    	set directly via -mcpu=<xxx>, rather than having separate -m<option>
    	arguments.
    	(rs6000_option_override_internal): Likewise.
    	(rs6000_coy_isa_masks): New list of CPU ISA options for debugging.
    	(rs6000_pragma_target_parse): Add support for CPU ISA options that are
    	set directly via -mcpu=<xxx>, rather than having separate -m<option>
    	arguments.
    	(rs6000_function_specific_print): Likewise.
    	(rs6000_print_options_internal): Likewise.
    	(rs6000_print_isa_options): Likewise.
    	* config/rs6000/rs6000.h (ASM_CPU_SPEC): Add support for -mcpu=future.
    	* config/rs6000/rs6000.opt (rs6000_cpu_isa_flags): New target global
    	variable.
    	(x_rs6000_cpu_isa_flags): Likewise.
    
    gcc/testsuite/
    
    	* gcc.target/powerpc/future-1.c: New test.
    	* gcc.target/powerpc/future-2.c: Likewise.

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -101,6 +101,7 @@
    you make changes here, make them also there.  */
 #define ASM_CPU_SPEC \
 "%{mcpu=native: %(asm_cpu_native); \
+  mcpu=future: -mfuture; \
   mcpu=power11: -mpower11; \
   mcpu=power10: -mpower10; \
   mcpu=power9: -mpower9; \
@@ -554,6 +555,12 @@ extern int rs6000_vector_align[];
    registers.  */
 #define TARGET_DIRECT_MOVE_64BIT	(TARGET_DIRECT_MOVE		\
 					 && TARGET_POWERPC64)
+
+/* ISA bits that are set via -mcpu=<xxx>, but that do not have an associated
+   switch with the option.  */
+#define CPU_ISA_MASK_FUTURE	0x1	/* -mcpu=future used.  */
+
+#define TARGET_FUTURE		(rs6000_cpu_isa_flags & CPU_ISA_MASK_FUTURE)
 
 /* Inlining allows targets to define the meanings of bits in target_info
    field of ipa_fn_summary by itself, the used bits for rs6000 are listed
