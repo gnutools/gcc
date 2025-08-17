b12ba214901a1676b9b040958de2119776c80ce8
Mikael Morin <mikael@gcc.gnu.org>
2025-08-17 19:43:40 +0200

    Reindentation retour à la ligne set_descriptor_with_shape

--- gcc/fortran/trans-descriptor.cc
+++ gcc/fortran/trans-descriptor.cc
@@ -1768,11 +1768,12 @@ gfc_set_descriptor_with_shape (stmtblock_t *block, tree desc, tree ptr,
 
   gfc_conv_expr (&shapese, shape);
   gfc_add_block_to_block (&body, &shapese.pre);
-  tree ubound = fold_build2_loc (
-    input_location, MINUS_EXPR, gfc_array_index_type,
-    fold_build2_loc (input_location, PLUS_EXPR, gfc_array_index_type, lbound,
-		     fold_convert (gfc_array_index_type, shapese.expr)),
-    gfc_index_one_node);
+  tmp = fold_build2_loc (input_location, MINUS_EXPR, gfc_array_index_type,
+			 lbound, gfc_index_one_node);
+  tree ubound = fold_build2_loc (input_location, PLUS_EXPR,
+				 gfc_array_index_type, tmp,
+				 fold_convert (gfc_array_index_type,
+					       shapese.expr));
   gfc_conv_descriptor_ubound_set (&body, desc, dim, ubound);
   gfc_add_block_to_block (&body, &shapese.post);
 
@@ -1784,10 +1785,11 @@ gfc_set_descriptor_with_shape (stmtblock_t *block, tree desc, tree ptr,
 				   gfc_array_index_type, offset, tmp));
 
   /* Update stride.  */
-  gfc_add_modify (
-    &body, stride,
-    fold_build2_loc (input_location, MULT_EXPR, gfc_array_index_type, stride,
-		     fold_convert (gfc_array_index_type, shapese.expr)));
+  gfc_add_modify (&body, stride,
+		  fold_build2_loc (input_location, MULT_EXPR,
+				   gfc_array_index_type, stride,
+				   fold_convert (gfc_array_index_type,
+						 shapese.expr)));
   /* Finish scalarization loop.  */
   gfc_trans_scalarizing_loops (&loop, &body);
   gfc_add_block_to_block (block, &loop.pre);
