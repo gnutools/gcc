aac28524d40fe9a19a49432ddc5e5fd763cd1cf2
Michael Meissner <meissner@linux.ibm.com>
2025-09-21 03:48:29 -0400

    Revert changes

--- gcc/config/rs6000/rs6000-c.cc
+++ gcc/config/rs6000/rs6000-c.cc
@@ -440,8 +440,6 @@ rs6000_target_modify_macros (bool define_p,
     rs6000_define_or_undefine_macro (define_p, "_ARCH_PWR10");
   if ((cpu_option & CPU_OPTION_POWER11_MASK) != 0)
     rs6000_define_or_undefine_macro (define_p, "_ARCH_PWR11");
-  if ((cpu_option & CPU_OPTION_FUTURE_MASK) != 0)
-    rs6000_define_or_undefine_macro (define_p, "_ARCH_FUTURE");
   if ((flags & OPTION_MASK_SOFT_FLOAT) != 0)
     rs6000_define_or_undefine_macro (define_p, "_SOFT_FLOAT");
   if ((flags & OPTION_MASK_RECIP_PRECISION) != 0)
