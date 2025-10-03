8eaed2a1cd9a6d043eeeb86dc488c0a996ec6b63
Mikael Morin <mikael@gcc.gnu.org>
2025-10-03 17:29:57 +0200

    Correction régression internal_readwrite_3.f90

--- libgfortran/io/unix.c
+++ libgfortran/io/unix.c
@@ -1079,7 +1079,7 @@ open_internal4 (char *base, size_t length, gfc_offset offset)
   s->buffer = base;
   s->buffer_offset = offset;
 
-  s->active = s->file_length = length;
+  s->active = s->file_length = length * sizeof (gfc_char4_t);
 
   s->st.vptr = &mem4_vtable;
 
