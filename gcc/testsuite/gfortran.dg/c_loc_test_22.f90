10eeb2df9d7430a83e34b25e1e11d7625f22009f
Mikael Morin <mikael@gcc.gnu.org>
2025-10-07 12:10:49 +0200

    Mise à jour dump c_loc_test_22.f90

--- gcc/testsuite/gfortran.dg/c_loc_test_22.f90
+++ gcc/testsuite/gfortran.dg/c_loc_test_22.f90
@@ -18,6 +18,6 @@ end
 ! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) &\\(.xxx.\[0-9\]+\\)\\\[0\\\];" 1 "original" } }
 ! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) &\\(.xxx.\[0-9\]+\\)\\\[D.\[0-9\]+ \\* 4\\\];" 1 "original" } }
 ! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) yyy.\[0-9\]+;" 1 "original" } }
-! { dg-final { scan-tree-dump-times "parm.\[0-9\]+.data = \\(void .\\) yyy.\[0-9\]+ \\+ \\(sizetype\\) \\(D.\[0-9\]+ \\* 16\\);" 1 "original" } }
+! { dg-final { scan-tree-dump-times {parm.[0-9]+\.data = \(void .\) yyy.[0-9]+ \+ \((?:sizetype|unsigned long)\) \(D.[0-9]+ \* 4\);} 1 "original" } }
 
 ! { dg-final { scan-tree-dump-times "D.\[0-9\]+ = parm.\[0-9\]+.data;\[^;]+ptr\[1-4\] = D.\[0-9\]+;" 4 "original" } }
