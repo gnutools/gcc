cba7f5c2ff3b6a4caea7be87581b22792a220d18
Michael Meissner <meissner@linux.ibm.com>
2025-11-03 18:57:23 -0500

    Add initial _Float16 and __bfloat16 support.
    2025-11-03  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/float16.md: New file.
    	* config/rs6000/predicats.md (easy_fp_constant): Add support for BFmode
    	and HFmode constants.
    	(fp16_xxspltiw_constant): New predicate.
    	* config/rs6000/rs6000-builtin.cc (rs6000_type_string): Add support for
    	BFmode and HFmode.
    	(rs6000_init_builtins): Create bfloat16_type_node and add support for
    	the __bfloat16 keyword.
    	* config/rs6000/rs6000-c.cc (rs6000_target_modify_macros): Define
    	__FLOAT16__ if _Float16 is supported.  Define __BFLOAT16__ if __bfloat16
    	is supported.
    	* config/rs6000/rs6000-call.cc (init_cumulative_args): Warn if 16-bit
    	floating point values are returned.
    	(rs6000_function_arg): Warning if 16-bit floating point values are
    	passed as arguments.
    	* config/rs6000/rs6000-protos.h (vec_const_128bit_type): Ad mode field
    	for supporting 16-bit floating point constants.
    	* config/rs6000/rs6000.cc (rs6000_hard_regno_mode_ok_uncached): Add
    	support for BFmode and HFmode.
    	(rs6000_modes_tieable_p): Likewise.
    	(rs6000_debug_reg_global): Likewise.
    	(rs6000_setup_reg_addr_masks): Likewise.
    	(rs6000_init_hard_regno_mode_ok): Likewise.
    	(rs6000_option_override_internal): Add checks for -mfloat16 and
    	-mbfloat16.
    	(easy_altivec_constant): Add support for HFmode and BFmode constants.
    	(rs6000_expand_vector_init): Likewise.
    	(reg_offset_addressing_ok_p): Restrict HFmode and BFmode to indexed
    	addressing.
    	(rs6000_legitimate_offset_address_p): Likewise.
    	(legitimate_lo_sum_address_p): Likewise.
    	(rs6000_secondary_reload_simple_move): Add support for HFmode and
    	BFmode.
    	(rs6000_preferred_reload_class): Likewise.
    	(rs6000_can_change_mode_class): Likewise.
    	(rs6000_load_constant_and_splat): Likewise.
    	(rs6000_scalar_mode_supported_p): Likewise.
    	(rs6000_floatn_mode): Likewise.
    	(rs6000_opt_masks): Add support for -mfloat16 and -mbfloat16 options.
    	(constant_fp_to_128bit_vector): Add support for HFmode and BFmode.
    	(constant_generates_xxspltiw): Likewise.
    	* config/rs6000/rs6000.h (FP16_SCALAR_MODE_P): New macro.
    	(FP16_VECTOR_MODE_P): Likewise.
    	* config/rs6000/rs6000.md (wd): Add HFmode and BFmode.
    	(toplevel): Include float16.md.
    	* config/rs6000/rs6000.opt (-mfloat16): New option.
    	(-mbfloat16): Likewise.

--- gcc/config/rs6000/rs6000.h
+++ gcc/config/rs6000/rs6000.h
@@ -343,6 +343,16 @@ extern const char *host_detect_local_cpu (int argc, const char **argv);
    || ((MODE) == TDmode)						\
    || (!TARGET_FLOAT128_TYPE && FLOAT128_IEEE_P (MODE)))
 
+/* Is this a valid 16-bit scalar floating point mode?  */
+#define FP16_SCALAR_MODE_P(MODE)					\
+  (((MODE) == HFmode && TARGET_FLOAT16)					\
+   || ((MODE) == BFmode && TARGET_BFLOAT16))
+
+/* Is this a valid 16-bit vector floating point mode?  */
+#define FP16_VECTOR_MODE_P(MODE)					\
+  (((MODE) == V8HFmode && TARGET_FLOAT16)				\
+   || ((MODE) == V8BFmode && TARGET_BFLOAT16))
+
 /* Return true for floating point that does not use a vector register.  */
 #define SCALAR_FLOAT_MODE_NOT_VECTOR_P(MODE)				\
   (SCALAR_FLOAT_MODE_P (MODE) && !FLOAT128_VECTOR_P (MODE))
