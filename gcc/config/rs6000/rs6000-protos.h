f360dd5eb80a8d477fb00157501708097911a39f
Michael Meissner <meissner@linux.ibm.com>
2025-11-10 13:45:38 -0500

    Revert changes

--- gcc/config/rs6000/rs6000-protos.h
+++ gcc/config/rs6000/rs6000-protos.h
@@ -250,7 +250,6 @@ typedef struct {
   bool all_words_same;			/* Are the words all equal?  */
   bool all_half_words_same;		/* Are the half words all equal?  */
   bool all_bytes_same;			/* Are the bytes all equal?  */
-  machine_mode mode;			/* Original constant mode.  */
 } vec_const_128bit_type;
 
 extern bool vec_const_128bit_to_bytes (rtx, machine_mode,
@@ -258,19 +257,6 @@ extern bool vec_const_128bit_to_bytes (rtx, machine_mode,
 extern unsigned constant_generates_lxvkq (vec_const_128bit_type *);
 extern unsigned constant_generates_xxspltiw (vec_const_128bit_type *);
 extern unsigned constant_generates_xxspltidp (vec_const_128bit_type *);
-
-/* From float16.cc.  */
-/* Optimize bfloat16 and float16 operations.  */
-enum fp16_operation {
-  FP16_BINARY,				/* Bfloat16/float16 binary op.  */
-  FP16_FMA,				/* (a * b) + c.  */
-  FP16_FMS,				/* (a * b) - c.  */
-  FP16_NFMA,				/* - ((a * b) + c).  */
-  FP16_NFMS				/* - ((a * b) - c).  */
-};
-
-extern void fp16_vectorization (enum rtx_code, rtx, rtx, rtx, rtx,
-				enum fp16_operation);
 #endif /* RTX_CODE */
 
 #ifdef TREE_CODE
