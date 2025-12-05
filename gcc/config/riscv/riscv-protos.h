1eac1cd96654745999d5831659ff9abb23d3d94e
Robin Dapp <rdapp@ventanamicro.com>
2025-12-05 10:55:18 +0100

    RISC-V: Pragma target [PR115325].
    This implements #pragma GCC target () for riscv.
    
    All the functionality was already there so we only need to wrap existing
    functions.
    
    	PR target/115325
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv-c.cc (riscv_pragma_target_parse): New
    	function.
    	(riscv_register_pragmas): Register riscv_pragma_target_parse.
    	* config/riscv/riscv-protos.h (riscv_process_target_attr_for_pragma):
    	Declare.
    	(riscv_reset_previous_fndecl): Ditto.
    	* config/riscv/riscv-target-attr.cc (riscv_process_target_attr_for_pragma):
    	New function.
    	* config/riscv/riscv.cc (riscv_reset_previous_fndecl): Reset.
    	(riscv_option_save): New function.
    	(riscv_option_print): Ditto.
    	(riscv_get_interrupt_type): Adjust docs.
    	(TARGET_OPTION_SAVE): Implement.
    	(TARGET_OPTION_PRINT): Ditto.
    	* doc/extend.texi: Document that riscv can do target pragams.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/pragma-target-1.c: New test.
    	* gcc.target/riscv/pragma-target-2.c: New test.
    
    (cherry picked from commit c139a4c91829373e77dedf8bb62f9e4361fc56ab)

--- gcc/config/riscv/riscv-protos.h
+++ gcc/config/riscv/riscv-protos.h
@@ -849,10 +849,13 @@ riscv_option_valid_attribute_p (tree, tree, tree, int);
 extern bool
 riscv_option_valid_version_attribute_p (tree, tree, tree, int);
 extern bool
+riscv_process_target_attr_for_pragma (tree);
+extern bool
 riscv_process_target_version_attr (tree, location_t *);
 extern void
 riscv_override_options_internal (struct gcc_options *);
 extern void riscv_option_override (void);
+extern void riscv_reset_previous_fndecl (void);
 extern rtx riscv_prefetch_cookie (rtx, rtx);
 extern bool riscv_prefetch_offset_address_p (rtx, machine_mode);
 
