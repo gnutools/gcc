a1f46029d8b841551ad116e32f0a9f796b01e5b5
Michael Meissner <meissner@linux.ibm.com>
2025-12-08 19:24:56 -0500

    Optimize __bfloat16 scalar code.
    Optimize __bfloat16 binary operations.  Unlike _Float16 where we
    have instructions to convert between HFmode and SFmode as scalar
    values, with BFmode, we only have vector conversions.  Thus to do:
    
    	__bfloat16 a, b, c;
    
    	a = b + c;
    
    the GCC compiler generates the following code:
    
    	lxsihzx 0,4,2           // load __bfloat16 value b
    	lxsihzx 12,5,2          // load __bfloat16 value c
    	xxsldwi 0,0,0,1         // shift b into bits 16..31
    	xxsldwi 12,12,12,1      // shift c into bits 16..31
    	xvcvbf16spn 0,0         // vector convert b into V4SFmode
    	xvcvbf16spn 12,12       // vector convert c into V4SFmode
    	xscvspdpn 0,0           // convert b into SFmode scalar
    	xscvspdpn 12,12         // convert c into SFmode scalar
    	fadds 0,0,12            // add b+c
    	xscvdpspn 0,0           // convert b+c into SFmode memory format
    	xvcvspbf16 0,0          // convert b+c into BFmode memory format
    	stxsihx 0,3,2           // store b+c
    
    Using the following combiner patterns that are defined in this patch, the code
    generated would be:
    
    	lxsihzx 12,4,2          // load __bfloat16 value b
    	lxsihzx 0,5,2           // load __bfloat16 value c
    	xxspltw 12,12,1         // shift b into bits 16..31
    	xxspltw 0,0,1           // shift c into bits 16..31
    	xvcvbf16spn 12,12       // vector convert b into V4SFmode
    	xvcvbf16spn 0,0         // vector convert c into V4SFmode
    	xvaddsp 0,0,12          // vector b+c in V4SFmode
    	xvcvspbf16 0,0          // convert b+c into BFmode memory format
    	stxsihx 0,3,2           // store b+c
    
    We cannot just define insns like 'addbf3' to keep the operation as
    BFmode because GCC will not generate these patterns unless the user
    uses -Ofast.  Without -Ofast, it will always convert BFmode into
    SFmode.
    
    2025-12-08  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	* config/rs6000/float16.cc (bfloat16_operation_as_v4sf): New function to
    	optimize __bfloat16 scalar operations.
    	* config/rs6000/float16.md (bfloat16_binary_op_internal1): New
    	__bfloat16 scalar combiner insns.
    	(bfloat16_binary_op_internal2): Likewise.
    	(bfloat16_fma_internal1): Likewise.
    	(bfloat16_fma_internal2): Likewise.
    	(bfloat16_fms_internal1): Likewise.
    	(bfloat16_fms_internal2): Likewise.
    	(bfloat16_nfma_internal1): Likewise.
    	(bfloat16_nfma_internal2): Likewise.
    	(bfloat16_nfms_internal3): Likewise.
    	* config/rs6000/predicates.md (fp16_reg_or_constant_operand): New
    	predicate.
    	(bfloat16_v4sf_operand): Likewise.
    	(bfloat16_bf_operand): Likewise.
    	* config/rs6000/rs6000-protos.h (bfloat16_operation_as_v4sf): New
    	declaration.

--- gcc/config/rs6000/float16.cc
+++ gcc/config/rs6000/float16.cc
@@ -183,3 +183,153 @@ fp16_vectorization (enum rtx_code icode,
 
   return;
 }
+
+/* Expand a bfloat16 scalar floating point operation:
+
+   ICODE:   Operation to perform.
+   RESULT:  Result of the operation.
+   OP1:     Input operand1.
+   OP2:     Input operand2.
+   OP3:     Input operand3 or NULL_RTX.
+   SUBTYPE: Describe the operation.
+
+   The operation is done as a V4SFmode vector operation.  This is because
+   converting BFmode from a scalar BFmode to SFmode to do the operation and
+   back again takes quite a bit of time.  GCC will only generate the native
+   operation if -Ofast is used.  The float16.md code that calls this function
+   adds various combine operations to do the operation in V4SFmode instead of
+   SFmode.  */
+	
+void
+bfloat16_operation_as_v4sf (enum rtx_code icode,
+			    rtx result,
+			    rtx op1,
+			    rtx op2,
+			    rtx op3,
+			    enum fp16_operation subtype)
+{
+  gcc_assert (can_create_pseudo_p ());
+
+  rtx result_v4sf = gen_reg_rtx (V4SFmode);
+  rtx ops_orig[3] = { op1, op2, op3 };
+  rtx ops_v4sf[3];
+  size_t n_opts;
+
+  switch (subtype)
+    {
+    case FP16_BINARY:
+      n_opts = 2;
+      gcc_assert (op3 == NULL_RTX);
+      break;
+
+    case FP16_FMA:
+    case FP16_FMS:
+    case FP16_NFMA:
+    case FP16_NFMS:
+      gcc_assert (icode == FMA);
+      n_opts = 3;
+      break;
+
+    default:
+      gcc_unreachable ();
+    }
+
+  for (size_t i = 0; i < n_opts; i++)
+    {
+      rtx op = ops_orig[i];
+      rtx tmp = ops_v4sf[i] = gen_reg_rtx (V4SFmode);
+
+      gcc_assert (op != NULL_RTX);
+
+      /* Remove truncation/extend added.  */
+      if (GET_CODE (op) == FLOAT_EXTEND || GET_CODE (op) == FLOAT_TRUNCATE)
+	op = XEXP (op, 0);
+
+      /* Convert operands to V4SFmode format.  We use SPLAT for registers to
+	 get the value into the upper 32-bits.  We can use XXSPLTW to splat
+	 words instead of VSPLTIH since the XVCVBF16SPN instruction ignores the
+	 odd half-words, and XXSPLTW can operate on all VSX registers instead
+	 of just the Altivec registers.  Using SPLAT instead of a shift also
+	 insure that other bits are not a signalling NaN.  If we are using
+	 XXSPLTIW or XXSPLTIB to load the constant the other bits are
+	 duplicated.  */
+
+      if (op == CONST0_RTX (SFmode) || op == CONST0_RTX (BFmode))
+	emit_move_insn (tmp, CONST0_RTX (V4SFmode));
+
+      else if (GET_MODE (op) == BFmode)
+	{
+	  emit_insn (gen_xxspltw_bf (tmp, force_reg (BFmode, op)));
+	  emit_insn (gen_xvcvbf16spn_bf (tmp, tmp));
+	}
+
+      else if (GET_MODE (op) == SFmode)
+	{
+	  if (GET_CODE (op) == CONST_DOUBLE)
+	    {
+	      rtvec v = rtvec_alloc (4);
+
+	      for (size_t i = 0; i < 4; i++)
+		RTVEC_ELT (v, i) = op;
+
+	      emit_insn (gen_rtx_SET (tmp,
+				      gen_rtx_CONST_VECTOR (V4SFmode, v)));
+	    }
+
+	  else
+	    emit_insn (gen_vsx_splat_v4sf (tmp,
+					   force_reg (SFmode, op)));
+	}
+
+      else
+	gcc_unreachable ();
+    }
+
+  /* Do the operation in V4SFmode.  */
+  switch (subtype)
+    {
+    case FP16_BINARY:
+      emit_insn (gen_rtx_SET (result_v4sf,
+			      gen_rtx_fmt_ee (icode, V4SFmode,
+					      ops_v4sf[0],
+					      ops_v4sf[1])));
+      break;
+
+    case FP16_FMA:
+    case FP16_FMS:
+    case FP16_NFMA:
+    case FP16_NFMS:
+      {
+	rtx op1 = ops_v4sf[0];
+	rtx op2 = ops_v4sf[1];
+	rtx op3 = ops_v4sf[2];
+
+	if (subtype == FP16_FMS || subtype == FP16_NFMS)
+	  op3 = gen_rtx_NEG (V4SFmode, op3);
+
+	rtx op_fma = gen_rtx_FMA (V4SFmode, op1, op2, op3);
+
+	if (subtype == FP16_NFMA || subtype == FP16_NFMS)
+	  op_fma = gen_rtx_NEG (V4SFmode, op_fma);
+
+	emit_insn (gen_rtx_SET (result_v4sf, op_fma));
+      }
+      break;
+
+    default:
+      gcc_unreachable ();
+    }
+
+  /* Convert V4SF result back to scalar mode.  */
+  if (GET_MODE (result) == BFmode)
+    emit_insn (gen_xvcvspbf16_bf (result, result_v4sf));
+
+  else if (GET_MODE (result) == SFmode)
+    {
+      rtx element = GEN_INT (WORDS_BIG_ENDIAN ? 2 : 3);
+      emit_insn (gen_vsx_extract_v4sf (result, result_v4sf, element));
+    }
+
+  else
+    gcc_unreachable ();
+}
