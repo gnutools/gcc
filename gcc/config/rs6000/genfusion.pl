61a5d4bc66351134acaf2960b4c2306bdb6ee738
Michael Meissner <meissner@linux.ibm.com>
2026-01-30 17:04:21 -0500

    PR target/117251: Improve vector nand to vector xor fusion
    See the following post for a complete explanation of what the patches
    for PR target/117251:
    
     * https://gcc.gnu.org/pipermail/gcc-patches/2025-June/686474.html
    
    This is patch #35 of 45 to generate the 'XXEVAL' instruction on power10
    and power11 instead of using the Altivec 'VNAND' instruction feeding
    into 'VXOR'.  The 'XXEVAL' instruction can use all 64 vector registers,
    instead of the 32 registers that traditional Altivec vector
    instructions use.  By allowing all of the vector registers to be used,
    it reduces the amount of spilling that a large benchmark generated.
    
    Currently the following code:
    
    	vector int a, b, c, d;
    	a = (~ (c & d)) ^ b;
    
    Generates:
    
    	vnand  t,c,d
    	vxor   a,t,b
    
    Now in addition with this patch, if the arguments or result is
    allocated to a traditional FPR register, the GCC compiler will now
    generate the following code instead of adding vector move instructions:
    
    	xxeval a,b,c,225
    
    Since fusion using 2 Altivec instructions is slightly faster than using
    the 'XXEVAL' instruction we prefer to generate the Altivec instructions
    if we can.  In addition, because 'XXEVAL' is a prefixed instruction, it
    possibly might generate an extra NOP instruction to align the 'XXEVAL'
    instruction.
    
    I have tested these patches on both big endian and little endian
    PowerPC servers, with no regressions.  Can I check these patchs into
    the trunk?
    
    2026-01-30  Michael Meissner  <meissner@linux.ibm.com>
    
    gcc/
    
    	PR target/117251
    	* config/rs6000/fusion.md: Regenerate.
    	* config/rs6000/genfusion.pl (gen_logical_addsubf): Add support
    	to generate vector nand => xor fusion if XXEVAL is supported.

--- gcc/config/rs6000/genfusion.pl
+++ gcc/config/rs6000/genfusion.pl
@@ -249,6 +249,7 @@ sub gen_logical_addsubf
       "vandc_vnor"  => 208,
       "vandc_veqv"  => 210,
       "vand_vnor"   => 224,
+      "vnand_vxor"  => 225,
     );
 
     KIND: foreach $kind ('scalar','vector') {
