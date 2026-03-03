0018f3410d513caf89593d98e8ab3f9c7c185037
Michael Meissner <meissner@linux.ibm.com>
2026-03-03 17:32:45 -0500

    Add support for 1,024 bit dense math registers.
    This patch is a prelimianry patch to add the full 1,024 bit dense math register
    (DMRs) for -mcpu=future.  The MMA 512-bit accumulators map onto the top of the
    DMR register.
    
    This patch only adds the new 1,024 bit register support.  It does not add
    support for any instructions that need 1,024 bit registers instead of 512 bit
    registers.
    
    I used the new mode 'TDOmode' to be the opaque mode used for 1,024 bit
    registers.  The 'wD' constraint added in previous patches is used for these
    registers.  I added support to do load and store of DMRs via the VSX registers,
    since there are no load/store dense math instructions.  I added the new keyword
    '__dm1024' to create 1,024 bit types that can be loaded into dense math
    registers.
    
    The patches have been tested on both little and big endian systems.  Can I check
    it into the master branch?
    
    This is version 4 of the patches.  The previous patches were:
    
     * https://gcc.gnu.org/pipermail/gcc-patches/2026-February/707452.html
     * https://gcc.gnu.org/pipermail/gcc-patches/2026-February/707453.html
     * https://gcc.gnu.org/pipermail/gcc-patches/2026-February/707454.html
     * https://gcc.gnu.org/pipermail/gcc-patches/2026-February/707455.html
     * https://gcc.gnu.org/pipermail/gcc-patches/2026-February/707456.html
    
    gcc/
    
    2026-03-03   Michael Meissner  <meissner@linux.ibm.com>
    
    	* config/rs6000/mma.md (UNSPEC_DM_INSERT512_UPPER): New unspec.
    	(UNSPEC_DM_INSERT512_LOWER): Likewise.
    	(UNSPEC_DM_EXTRACT512): Likewise.
    	(UNSPEC_DM_RELOAD_FROM_MEMORY): Likewise.
    	(UNSPEC_DM_RELOAD_TO_MEMORY): Likewise.
    	(movtdo): New define_expand and define_insn_and_split to implement 1,024
    	bit dense math registers.
    	(movtdo_insert512_upper): New insn.
    	(movtdo_insert512_lower): Likewise.
    	(movtdo_extract512): Likewise.
    	(reload_tdo_from_memory): Likewise.
    	(reload_tdo_to_memory): Likewise.
    	* config/rs6000/rs6000-builtin.cc (rs6000_type_string): Add dense math
    	register support.
    	(rs6000_init_builtins): Add support for __dm1024 keyword.
    	* config/rs6000/rs6000-call.cc (rs6000_return_in_memory): Add support
    	for TDOmode.
    	(rs6000_function_arg): Likewise.
    	* config/rs6000/rs6000-modes.def (TDOmode): New mode.
    	* config/rs6000/rs6000.cc (rs6000_hard_regno_nregs_internal): Add
    	support for TDOmode.
    	(rs6000_hard_regno_mode_ok_uncached): Likewise.
    	(rs6000_hard_regno_mode_ok): Likewise.
    	(rs6000_modes_tieable_p): Likewise.
    	(rs6000_debug_reg_global): Likewise.
    	(rs6000_setup_reg_addr_masks): Likewise.
    	(rs6000_init_hard_regno_mode_ok): Add support for TDOmode.  Setup reload
    	hooks for dense math TDO reload mode.
    	(reg_offset_addressing_ok_p): Add support for TDOmode.
    	(rs6000_emit_move): Likewise.
    	(rs6000_secondary_reload_simple_move): Likewise.
    	(rs6000_preferred_reload_class): Likewise.
    	(rs6000_mangle_type): Add mangling for __dm1024 type.
    	(rs6000_dense_math_register_move_cost): Add support for TDOmode.
    	(rs6000_split_multireg_move): Likewise.
    	(rs6000_invalid_conversion): Likewise.
    	* config/rs6000/rs6000.h (VECTOR_ALIGNMENT_P): Add TDOmode.
    	(enum rs6000_builtin_type_index): Add dense math register type nodes.
    	(dm1024_type_node): Likewise.
    	(ptr_dm1024_type_node): Likewise.
    
    gcc/testsuite/
    
    2026-03-03   Michael Meissner  <meissner@linux.ibm.com>
    
    	* gcc.target/powerpc/dm-1024bit.c: New test.

--- gcc/config/rs6000/rs6000-builtin.cc
+++ gcc/config/rs6000/rs6000-builtin.cc
@@ -495,6 +495,8 @@ const char *rs6000_type_string (tree type_node)
     return "__vector_pair";
   else if (type_node == vector_quad_type_node)
     return "__vector_quad";
+  else if (type_node == dm1024_type_node)
+    return "__dm1024";
 
   return "unknown";
 }
@@ -780,6 +782,21 @@ rs6000_init_builtins (void)
 					  "__vector_quad");
   t = build_qualified_type (vector_quad_type_node, TYPE_QUAL_CONST);
   ptr_vector_quad_type_node = build_pointer_type (t);
+
+  /* For TDOmode (1,024 bit dense math accumulators), don't use an alignment of
+     1,024, use 512.  TDOmode loads and stores are always broken up into 2
+     vector pair loads or stores.  In addition, we don't have support for
+     aligning the stack to 1,024 bits.  */
+  dm1024_type_node = make_node (OPAQUE_TYPE);
+  SET_TYPE_MODE (dm1024_type_node, TDOmode);
+  TYPE_SIZE (dm1024_type_node) = bitsize_int (GET_MODE_BITSIZE (TDOmode));
+  TYPE_PRECISION (dm1024_type_node) = GET_MODE_BITSIZE (TDOmode);
+  TYPE_SIZE_UNIT (dm1024_type_node) = size_int (GET_MODE_SIZE (TDOmode));
+  SET_TYPE_ALIGN (dm1024_type_node, 512);
+  TYPE_USER_ALIGN (dm1024_type_node) = 0;
+  lang_hooks.types.register_builtin_type (dm1024_type_node, "__dm1024");
+  t = build_qualified_type (dm1024_type_node, TYPE_QUAL_CONST);
+  ptr_dm1024_type_node = build_pointer_type (t);
 
   tdecl = add_builtin_type ("__bool char", bool_char_type_node);
   TYPE_NAME (bool_char_type_node) = tdecl;
