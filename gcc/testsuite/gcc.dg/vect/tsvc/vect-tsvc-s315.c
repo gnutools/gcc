c5cb57b4d8551e7c7fef96797ec7dd9ac30a2716
Alexandre Oliva <oliva@adacore.com>
2026-03-10 04:20:29 -0300

    testsuite: vect: ppc: add vect_no_int_remainder
    vect-tsvc-s315.c fails on powerpc with altivec only because it doesn't
    support vectorized remainder on type int.
    
    Introduce vect_no_int_remainder and require it.
    
    
    for  gcc/ChangeLog
    
    	* sourcebuild.texi (vect_no_int_remainder): Document.
    
    for  gcc/testsuite/ChangeLog
    
    	* lib/target-supports.exp
    	(check_effective_target_vect_no_int_remainder): New.
    	* gcc.dg/vect/tsvc/vect-tsvc-s315.c: Don't expect
    	vectorization without support for that.

--- gcc/testsuite/gcc.dg/vect/tsvc/vect-tsvc-s315.c
+++ gcc/testsuite/gcc.dg/vect/tsvc/vect-tsvc-s315.c
@@ -47,4 +47,4 @@ int main (int argc, char **argv)
   return 0;
 }
 
-/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" } } */
+/* { dg-final { scan-tree-dump-times "vectorized 1 loops" 1 "vect" { target { ! { vect_no_int_remainder } } } } } */
